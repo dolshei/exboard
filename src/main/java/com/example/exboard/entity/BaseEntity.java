121fab0ad9ae6e18e546451bd54d35a0220ab5bf
LEE.CHANG.HUN <chlee@kimzipsa.co.kr>
2022-08-04 17:41:07 +0900

    Initial commit

---
+++ src/main/java/com/example/exboard/entity/BaseEntity.java
@@ -0,0 +1,2 @@
+package com.example.exboard.entity;public class BaseEntity {
+}
---
+++ src/test/java/com/example/exboard/ExboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
