8054e36a6a830eba81b3158a30b8703e5470b7bf
LEE.CHANG.HUN <dolshei@gmail.com>
2022-09-04 17:33:20 +0900

    JPQLQuery를 사용한 검색

--- src/main/java/com/example/exboard/repository/search/SearchBoardRepository.java
+++ src/main/java/com/example/exboard/repository/search/SearchBoardRepository.java
@@ -1,7 +1,13 @@
 package com.example.exboard.repository.search;
 
 import com.example.exboard.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface SearchBoardRepository {
     Board search1();
+
+    // 원하는 파라미터(Pageable)를 전송하고 Page<Object[]>를 만들어 반환한다.
+    // PageRequestDTO 자체를 파라미터로 처리하지 않는 이유 : DTO를 가능하면 Repository 영역에서 다루지 않기 위해
+    Page<Object[]> searchPage(String type, String keyword, Pageable pageable);
 }
--- src/main/java/com/example/exboard/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/com/example/exboard/repository/search/SearchBoardRepositoryImpl.java
@@ -1,8 +1,19 @@
 package com.example.exboard.repository.search;
 
 import com.example.exboard.entity.Board;
+import com.example.exboard.entity.QBoard;
+import com.example.exboard.entity.QMember;
+import com.example.exboard.entity.QReply;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
 
 /**
  * 구현 클래스에서 가장 중요한 것 : QuerydslRepositorySupport 클래스를 상속해야 하는 것
@@ -17,7 +28,73 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
 
     @Override
     public Board search1() {
-        log.info("search1...................");
+        // log.info("search1.........");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        //jpqlQuery.select(board).where(board.bno.eq(2L));
+        //List<Board> result = jpqlQuery.fetch();
+
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+        tuple.groupBy(board);
+
+        List<Tuple> result = tuple.fetch();
+
         return null;
     }
+
+    @Override
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        BooleanExpression expression = board.bno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if (type != null) {
+            String[] typeArr = type.split("");
+
+            BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+            for (String t : typeArr) {
+                switch (t){
+                    case "t" :
+                        conditionBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "w" :
+                        conditionBuilder.or(member.email.contains(keyword));
+                        break;
+                    case "c" :
+                        conditionBuilder.or(board.content.contains(keyword));
+                        break;
+                }
+            }
+            booleanBuilder.and(conditionBuilder);
+        }
+
+        tuple.where(booleanBuilder);
+        tuple.groupBy(board);
+
+        List<Tuple> result = tuple.fetch();
+
+        return null;
+    }
+
+
 }
