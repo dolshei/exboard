6dd65055ca7ee75db37d8b0e058bd063cb64734f
LEE.CHANG.HUN <dolshei@gmail.com>
2022-09-10 13:36:43 +0900

    조회화면에 게시물의 댓글 수 추가

---
+++ src/main/java/com/example/exboard/controller/ReplyController.java
@@ -0,0 +1,28 @@
+package com.example.exboard.controller;
+
+import com.example.exboard.dto.ReplyDTO;
+import com.example.exboard.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/replies/")
+@RequiredArgsConstructor
+public class ReplyController {
+    @Autowired
+    private final ReplyService replyService;
+
+    @GetMapping(value = "/board/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno) {
+        return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+}
